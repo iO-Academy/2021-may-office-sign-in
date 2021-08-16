7b8283b9bdc544b5d9b3bc4070462e79875548ea
Lucy Ferrabee <lucy.ferrabee@hotmail.com>
2021-08-16 11:26:05 +0100

    added the db with dummy content

---
+++ db/BonjoursDB_2021-08-16.sql
@@ -0,0 +1,99 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 5446
+#
+# https://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.34)
+# Database: BonjoursDB
+# Generation Time: 2021-08-16 10:25:12 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table admins
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `admins`;
+
+CREATE TABLE `admins` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `surname` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(254) NOT NULL DEFAULT '',
+  `passcode` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+
+INSERT INTO `admins` (`id`, `name`, `surname`, `email`, `passcode`)
+VALUES
+	(1,'Monica','Bronica','monnybronny@bonjours.com',1111),
+	(2,'Bradley','Shmadley','bradders@bonjours.com',1111),
+	(3,'Sarah','Pallbearer','sassyk@bonjours.com',1111),
+	(4,'Ali','Pally','ali@bonjours.com',1111),
+	(5,'Jesus','Shmesus','jessyj@bonjours.com',1111),
+	(6,'David','Jones','david@bonjours.com',1111),
+	(7,'Betty','Spaghetti','bdog@bonjours.com',1111),
+	(8,'Bill','Bill','billsbillsbills@bonjours.com',1111),
+	(9,'Mike','Oram','mike@bonjours.com',1111),
+	(10,'Sandy','Dandy','dandy@bonjours.com',1111);
+
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table visitors
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `visitors`;
+
+CREATE TABLE `visitors` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `surname` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(254) NOT NULL DEFAULT '',
+  `company` varchar(120) DEFAULT NULL,
+  `entry_time` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `visitors` WRITE;
+/*!40000 ALTER TABLE `visitors` DISABLE KEYS */;
+
+INSERT INTO `visitors` (`id`, `name`, `surname`, `email`, `company`, `entry_time`)
+VALUES
+	(1,'Lucy','Ferrabee','lucy.test@hotmail.com','Microsoft',1629109360),
+	(2,'Fred','Perry','fred123@gmail.com','Just Eat',1629109480),
+	(3,'Sonia','Larson','larry@hotmail.com',NULL,1629109200),
+	(4,'Benedict','Cumberbatch','benten@uber.com','Uber',1629109670),
+	(5,'Penny','Lane','penny@lane.com','Lane Services Ltd.',1629109100),
+	(6,'Michael','Michaelson','mkay@me.com',NULL,1629109381),
+	(7,'Jonny','Begood','birdwatcher5376@gousto.com','Gousto',1629109427),
+	(8,'Samuel','Smith','smithy@gmail.com',NULL,1629110400),
+	(9,'Janet','Jackson','plainjane1999@hotmail.com',NULL,1629115000),
+	(10,'Sally','Silly','sal@gmail.com','Silly Sally Ltd.',1629109361);
+
+/*!40000 ALTER TABLE `visitors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

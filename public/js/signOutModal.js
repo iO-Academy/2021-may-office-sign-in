f4627a6137ee9a2b7c3395c27d4587a9cce1362a
SniffleSneeze <70772425+SniffleSneeze@users.noreply.github.com>
2021-08-19 09:33:22 +0100

    signout visitor page + validation + logic NEEd TO DO THE ADMIN SIGN OUT

--- public/js/signOutModal.js
+++ public/js/signOutModal.js
@@ -2,7 +2,6 @@ const close = document.querySelector('.closeSearch')
 const model = document.querySelector('#signOutName')
 const input = document.querySelector('#hiddenInput')
 
-
 close.addEventListener('click', (e) => {
     model.textContent = e.target.dataset.name;
     input.value = close.dataset.name;
--- src/Models/VisitorModel.php
+++ src/Models/VisitorModel.php
@@ -6,7 +6,6 @@ class VisitorModel
 {
     private $db;
 
-
     /**
      * @param $db
      */
